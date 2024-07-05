a704fb282c43669efd1f76664a1c4bd3147c39e7
sforteva <forteva@staff.uni-marburg.de>
2024-07-05 13:08:16 +0200

    translate comment

--- main.R
+++ main.R
@@ -1,6 +1,6 @@
 # Author: Spaska Forteva
 # Description: This function formats RADOLAN precipitation data for a database.
-# Main script to loads the RADOLAN data, processes it, and writes the formatted data to CSV files for each plotID.
+# Main script to load RADOLAN data, process it, and write formatted data to CSV files for each plotID.
 # Last change: 2024-07-05 Spaska Forteva
 
 library(sp)
@@ -12,23 +12,24 @@ library(stringr)
 
 args <- commandArgs(trailingOnly = TRUE)
 
-# Definiere die Parameter basierend auf den Argumenten
+# Define parameters based on arguments
 ftp_root <- args[1]
 year <- as.integer(args[2])
 monthEnd <- as.integer(args[3])
 main_directory <- args[4]
 monthStart <- as.integer(args[5])
 
-if(monthStart=="" || monthStart == 0) {
+if (monthStart == "" || monthStart == 0) {
   monthStart = 1
 }
 if (monthEnd == "" || monthEnd == 0) {
   monthEnd <- month(Sys.Date()) - 1
-if (monthEnd < 1) {
+  if (monthEnd < 1) {
     monthEnd <- 12
+  }
 }
 
-# Definieren der Parameter für den Aufruf der Funktion
+# Define parameters for function call
 #ftp_root <- "ftp://opendata.dwd.de/climate_environment/CDC/"
 #year = 2024
 #monthEnd = 6
@@ -36,23 +37,24 @@ if (monthEnd < 1) {
 #monthStart = 6
 
 ############# Step 1 ###################
-# Laden der Funktion aus dem Skript Skript1.R
+# Load function from script Skript1.R
 source(file.path(main_directory, "src", "00_downloadTheFiles.R"))
-# Aufrufen der Funktion download_RADOLAN_files mit den Parametern
-# Schleife über die Monate 1 bis 6
+# Call download_RADOLAN_files function with parameters
+# Loop over months 1 to 6
 for (month in monthStart:monthEnd) {
   download_RADOLAN_files(ftp_root, year, month, main_directory)
 }
 
 ############# Step 2 ###################
-# Aufrufen des zweiten Skripts mit Parametern Skript2.R
+# Call second script with parameters Skript2.R
 source(file.path(main_directory, "src", "01_extractRadolan.R"))
 for (month in monthStart:monthEnd) {
   datapath = main_directory
   process_RADOLAN_data(datapath, year, month)
 }
+
 ############# Step 3 ###################
-# Aufrufen des zweiten Skripts mit Parametern Skript3.R
+# Call third script with parameters Skript3.R
 source(file.path(main_directory, "src", "02_combineRadolan.R"))
 outpath = main_directory
 for (month in monthStart:monthEnd) {
@@ -60,30 +62,29 @@ for (month in monthStart:monthEnd) {
 }
 
 ############# Step 4 ###################
-# Aufrufen des zweiten Skripts mit Parametern Skript4.R
+# Call fourth script with parameters Skript4.R
 source(file.path(main_directory, "src", "04_reformat_for_DB.R"))
 mainpath = main_directory
 for (month in monthStart:monthEnd) {
   format_radolan_for_database(mainpath, year, month) 
 }
 
-
 base_dir <- file.path(main_directory, "data_extracted/", year, monthEnd, "/")
-stations <-""
-# Funktion zum Ermitteln der Stationen aus den Dateinamen
+
+# Function to determine stations from file names
 get_stations <- function(base_dir) {
   files <- list.files(path = base_dir, recursive = TRUE, pattern = "\\.csv$", full.names = TRUE)
   stations <- unique(str_extract(basename(files), "^[^\\.]+"))
   return(stations)
 }
 
-# Liste der Monate
+# List of months
 months <- sprintf("%02d", monthStart:monthEnd)
 
-# Liste der Stationen automatisch generieren
+# Automatically generate list of stations
 stations <- get_stations(base_dir)
 
-# Funktion zum Einlesen und Zusammenführen der CSV-Dateien pro Station
+# Function to read and merge CSV files per station
 process_station <- function(station) {
   station_data <- map_dfr(months, function(month) {
     file_path <- file.path(base_dir, month, paste0(station, ".csv"))
@@ -94,20 +95,15 @@ process_station <- function(station) {
     }
   })
   
-  actual_data_url <- file.path(main_directory, "data_extracted/", year, paste0(monthStart,"_",monthEnd))
+  actual_data_url <- file.path(main_directory, "data_extracted/", year, paste0(monthStart, "_", monthEnd))
   if (!dir.exists(actual_data_url)) {
     dir.create(actual_data_url)
-    cat("Leerer Ordner erstellt:", actual_data_url, "\n")
+    cat("Created empty directory:", actual_data_url, "\n")
   } else {
-    cat("Ordner existiert bereits:", actual_data_url, "\n")
+    cat("Directory already exists:", actual_data_url, "\n")
   }
   write_csv(station_data, file.path(actual_data_url, paste0(station, ".csv")))
 }
 
-# Verarbeite jede Station
+# Process each station
 walk(stations, process_station)
-
-
-# Verarbeite jede Station
-walk(stations, process_station)
-
